2d72d7aa90d9a4f43862a60e049c26e36e71bd07
fidellim <lim.fidel@gmail.com>
2022-11-30 18:50:52 +0400

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="top-footer"></div>
+            <div className="footer-container">
+                <ul>
+                    <li>Home</li>
+                    <li>Tickets</li>
+                    <li>Solutions</li>
+                </ul>
+                <h2>© 2022 Freshdesk DemoPh</h2>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
