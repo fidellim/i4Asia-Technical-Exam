032376c192f929d719e6dba4688b01a6e7012110
fidellim <lim.fidel@gmail.com>
2022-12-01 14:34:22 +0400

    add data in ticket status page

--- src/pages/TicketStatus.jsx
+++ src/pages/TicketStatus.jsx
@@ -1,7 +1,75 @@
 import React from 'react'
 
+const ticketData = [
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+    {
+        title: 'need a ticket sample #280',
+        timestamp: new Date(),
+        author: 'Paul Sonda',
+        status: 'closed',
+    },
+]
+
 const TicketStatus = () => {
-    return <div>TicketStatus</div>
+    return (
+        <div>
+            <h1>Ticket Status</h1>
+
+            {ticketData.map((ticket) => (
+                <div>
+                    <div>
+                        <h3>{ticket.title}</h3>
+                        <p>
+                            Created on {ticket.timestamp.toString()} Agent:{' '}
+                            {ticket.author}
+                        </p>
+                    </div>
+                    <div>{ticket.status}</div>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default TicketStatus
