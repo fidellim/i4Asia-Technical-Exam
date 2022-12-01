5ea032bf361ceea12a81e90dab4367b344143e16
fidellim <lim.fidel@gmail.com>
2022-12-02 01:45:52 +0400

    update ticket status page component

--- src/pages/TicketStatus.jsx
+++ src/pages/TicketStatus.jsx
@@ -1,55 +1,68 @@
-import React from 'react'
+import Ticket from '../components/Ticket'
+import Dropdown from '../components/Dropdown'
+import exportFile from '../assets/export-file.png'
 
 const ticketData = [
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
     {
         title: 'need a ticket sample #280',
-        timestamp: new Date(),
+        timestamp: 'Wed, Apr 13 at 4:30 PM',
         author: 'Paul Sonda',
         status: 'closed',
     },
 ]
+
+const viewDropdownList = {
+    title: 'VIEW',
+    current: 'All Tickets',
+    list: ['lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum'],
+}
+const sortDropdownList = {
+    title: 'SORT BY',
+    current: 'Date Created',
+    list: ['lorem ipsum', 'lorem ipsum', 'lorem ipsum', 'lorem ipsum'],
+}
 
 const TicketStatus = () => {
     return (
@@ -57,21 +70,26 @@ const TicketStatus = () => {
             <div className="ts-top-container">
                 <div className="ts-top">
                     <h1>Ticket Status</h1>
+
+                    <div className="ts-top-btns">
+                        <div className="dropdowns-container">
+                            <Dropdown data={viewDropdownList} />
+                            <Dropdown data={sortDropdownList} />
+                        </div>
+                        <button className="btn export-ticket-white-btn">
+                            <img src={exportFile} alt="" />
+                            Export Ticket
+                        </button>
+                    </div>
                 </div>
             </div>
-
-            {ticketData.map((ticket, index) => (
-                <div key={index}>
-                    <div>
-                        <h3>{ticket.title}</h3>
-                        <p>
-                            Created on {ticket.timestamp.toString()} Agent:{' '}
-                            {ticket.author}
-                        </p>
-                    </div>
-                    <div>{ticket.status}</div>
+            <div className="ts-bottom-container">
+                <div className="ts-bottom">
+                    {ticketData.map((ticket, index) => (
+                        <Ticket key={index} data={ticket} />
+                    ))}
                 </div>
-            ))}
+            </div>
         </div>
     )
 }
