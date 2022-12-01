dd46bde5beb2e73ab6ece1667eecade869ed939f
fidellim <lim.fidel@gmail.com>
2022-12-01 21:44:04 +0400

    make component reusable

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,16 +1,21 @@
 import book from '../assets/book.png'
+import { useLocation } from 'react-router-dom'
 
 const HomeCard = ({ data }) => {
+    const { pathname } = useLocation()
+
     return (
-        <div className="homeCard">
+        <div className={`homeCard ${pathname === '/' ? 'min-h-350' : ''}`}>
             <div className="homeCard-top">
                 <h3>{data.title}</h3>
                 <img src={data.icon} alt="" />
             </div>
             <div className="homeCard-bottom">
-                {data.topics.map((topic) => (
-                    <div key={topic.title} className="topic-container">
-                        <h3 className="topic-number">{topic.number}</h3>
+                {data.topics.map((topic, index) => (
+                    <div key={`${topic} ${index}`} className="topic-container">
+                        {topic.number && (
+                            <h3 className="topic-number">{topic.number}</h3>
+                        )}
                         <div className="topic-subheading-container">
                             <h4 className="topic-title">{topic.title}</h4>
                             {topic.subheadings.map((subheading, index) => (
@@ -19,7 +24,13 @@ const HomeCard = ({ data }) => {
                                     className="topic-subheading"
                                 >
                                     <img src={book} alt="" />
-                                    <a>{subheading}</a>
+                                    <a
+                                        className={`${
+                                            pathname === '/' ? 'max-w-150' : ''
+                                        }`}
+                                    >
+                                        {subheading}
+                                    </a>
                                 </div>
                             ))}
                         </div>
